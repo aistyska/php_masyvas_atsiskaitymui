0060c9d43001bcb72476c35153eab726325a4042
Aiste Laugalyte <aistyska@gmail.com>
2020-04-03 13:14:18 +0300

    Added HTML and students data

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>PHP masyvas</title>
+</head>
+<body>
+
 <?php
 
 function student_code()
@@ -17,10 +26,6 @@ function student_mark()
     return rand(1, 10);
 }
 
-
-
-echo student_code() . student_mark();
-echo input_date();
 
 $students = [
     "5a" => [
@@ -144,3 +149,31 @@ $students = [
         ]
     ]
 ];
+?>
+
+<table>
+    <tr>
+        <th>Klasė</th>
+        <th>Kodas</th>
+        <th>Vardas</th>
+        <th>Pavardė</th>
+        <th>Kontrolinių darbų vidurkis</th>
+        <th>Duomenų formavimo data</th>
+    </tr>
+
+<?php
+foreach ($students as $class => $all) {
+    foreach ($all as $student) :?>
+        <tr>
+            <td> <?= $class; ?> </td>
+            <td> <?= $student["code"] ?></td>
+            <td> <?= mb_strtoupper($student["first_name"]) ?> </td>
+            <td> <?= mb_strtoupper($student["last_name"]) ?> </td>
+            <td><?= round(array_sum($student["marks"]) / count($student["marks"])) ?> </td>
+            <td> <?= $student["date"] ?> </td>
+        </tr>
+<?php endforeach;
+    }?>
+</table>
+</body>
+</html>
